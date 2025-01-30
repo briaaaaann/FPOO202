66a3ba333f8d4caa7b8e8e737cfbc3921e90f1b5
briaaaaann <123046511@upq.edu.mx>
2025-01-30 15:20:14 -0600

    Correccion programa de palabras palindromas, ahora detecta frases y elimina espacios

--- practica_Cuatro/src/palindromo.java
+++ practica_Cuatro/src/palindromo.java
@@ -4,8 +4,9 @@ public class palindromo {
         Scanner sc = new Scanner(System.in);
         System.out.println("Dame una palabra: ");
         String palabra = sc.nextLine();
-        String invertida = new StringBuilder(palabra).reverse().toString();
-        if (palabra.equals(invertida)) {
+        String palabra2 = palabra.replaceAll(" ", "");
+        String invertida = new StringBuilder(palabra2).reverse().toString();
+        if (palabra2.equals(invertida)) {
             System.out.println("Palabra palindroma");
         }else{
             System.out.println("Palabra no palindroma");
