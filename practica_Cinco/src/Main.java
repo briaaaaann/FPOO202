4abadccda2494bf40d7c29d8e096954a3112ba38
briaaaaann <123046511@upq.edu.mx>
2025-01-31 19:26:47 -0600

    Creacion del Main y un programa que imprima desde 1 a N

---
+++ practica_Cinco/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        int num = sc.nextInt();
+        for(int i = 1;i <=num;i++){
+            System.out.print(i + ",");
+        }
+    }
+}
