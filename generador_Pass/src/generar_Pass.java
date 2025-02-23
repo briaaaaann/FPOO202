8853f9e51e8a7def700971055a82c9a3bff07686
briaaaaann <123046511@upq.edu.mx>
2025-02-22 20:40:17 -0600

    se creo un programa que genera una contraseña usando letras, numero y caracteres especiales, representado visualmete con el apoyo de JOption

---
+++ generador_Pass/src/generar_Pass.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class generar_Pass {
+    public int lenght = 8;
+    public boolean include_May;
+    public boolean caract_Esp;
+    public static String NUMEROS = "0123456789";
+    public static String MAYUSCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String MINUSCULAS = "abcdefghijklmnopqrstuvwxyz";
+    public static String ESPECIALES = "!@#$%&*?_-";
+    public String getPassword() {
+        String caracteres = NUMEROS + MINUSCULAS;
+        if (include_May) caracteres += MAYUSCULAS;
+        if (caract_Esp) caracteres += ESPECIALES;
+        String pswd = "";
+        for (int i = 0; i < lenght; i++) {
+            pswd += caracteres.charAt((int) (Math.random() * caracteres.length()));
+        }
+
+        return pswd;
+    }
+    public void mostrarPassword() {
+        String mensaje = "Contraseña generada con éxito\n\nLa contraseña es:\n" + getPassword();
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+}
