1e1860fef5afcf93ecbf831f5efc5ebc1938e5f4
briaaaaann <123046511@upq.edu.mx>
2025-02-27 15:27:20 -0600

    Modificamos el generador de contrasenias agregando constructores y encapsulamiento

--- generador_Pass/src/Main.java
+++ generador_Pass/src/Main.java
@@ -2,15 +2,16 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        generar_Pass pass = new generar_Pass();
+
         String longitud = JOptionPane.showInputDialog("Ingresa la Longitud de la contraseña:");
+        int lenght = Integer.parseInt(longitud);
 
         if (longitud == null || longitud.trim().isEmpty()) {
             JOptionPane.showMessageDialog(null, "Longitud no válida.");
             return;
         }
         if (longitud.matches("\\d+")) {
-            pass.lenght = Integer.parseInt(longitud);
+            lenght = Integer.parseInt(longitud);
         } else {
             JOptionPane.showMessageDialog(null, "Por favor, ingresa un número válido.");
             return;
@@ -23,12 +24,15 @@ public class Main {
         int option = JOptionPane.showConfirmDialog(null, message, "Confirmación", JOptionPane.OK_CANCEL_OPTION);
 
         if (option == JOptionPane.OK_OPTION) {
-            pass.include_May = checkBox.isSelected();
-            pass.caract_Esp = checkBoxEsp.isSelected();
+            //int option = JOptionPane.showConfirmDialog(null, message, "Confirmación", JOptionPane.OK_CANCEL_OPTION);
         } else {
             JOptionPane.showMessageDialog(null, "Operación cancelada.");
             return;
         }
+        boolean checked = checkBox.isSelected();
+        boolean checkedEsp = checkBoxEsp.isSelected();
+
+        generar_Pass pass = new generar_Pass(lenght, checked, checkedEsp);
 
         pass.mostrarPassword();
     }
--- generador_Pass/src/generar_Pass.java
+++ generador_Pass/src/generar_Pass.java
@@ -1,13 +1,20 @@
 import javax.swing.*;
 
 public class generar_Pass {
-    public int lenght = 8;
-    public boolean include_May;
-    public boolean caract_Esp;
-    public static String NUMEROS = "0123456789";
-    public static String MAYUSCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static String MINUSCULAS = "abcdefghijklmnopqrstuvwxyz";
-    public static String ESPECIALES = "!@#$%&*?_-";
+    private int lenght = 8;
+    private boolean include_May;
+    private boolean caract_Esp;
+    private static String NUMEROS = "0123456789";
+    private static String MAYUSCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String MINUSCULAS = "abcdefghijklmnopqrstuvwxyz";
+    private static String ESPECIALES = "!@#$%&*?_-";
+
+    public generar_Pass(int lenght, boolean include_May, boolean caract_Esp) {
+        this.lenght = lenght;
+        this.include_May = include_May;
+        this.caract_Esp = caract_Esp;
+    }
+
     public String getPassword() {
         String caracteres = NUMEROS + MINUSCULAS;
         if (include_May) caracteres += MAYUSCULAS;
@@ -22,6 +29,72 @@ public class generar_Pass {
     public void mostrarPassword() {
         String mensaje = "Contraseña generada con éxito\n\nLa contraseña es:\n" + getPassword();
         JOptionPane.showMessageDialog(null, mensaje);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    public boolean isInclude_May() {
+        return include_May;
+    }
+
+    public void setInclude_May(boolean include_May) {
+        this.include_May = include_May;
+    }
+
+    public boolean isCaract_Esp() {
+        return caract_Esp;
+    }
+
+    public void setCaract_Esp(boolean caract_Esp) {
+        this.caract_Esp = caract_Esp;
+    }
+
+    public static String getNUMEROS() {
+        return NUMEROS;
+    }
+
+    public static void setNUMEROS(String NUMEROS) {
+        generar_Pass.NUMEROS = NUMEROS;
+    }
+
+    public static String getMAYUSCULAS() {
+        return MAYUSCULAS;
+    }
+
+    public static void setMAYUSCULAS(String MAYUSCULAS) {
+        generar_Pass.MAYUSCULAS = MAYUSCULAS;
+    }
+
+    public static String getMINUSCULAS() {
+        return MINUSCULAS;
+    }
+
+    public static void setMINUSCULAS(String MINUSCULAS) {
+        generar_Pass.MINUSCULAS = MINUSCULAS;
+    }
+
+    public static String getESPECIALES() {
+        return ESPECIALES;
+    }
+
+    public static void setESPECIALES(String ESPECIALES) {
+        generar_Pass.ESPECIALES = ESPECIALES;
     }
 }
 
